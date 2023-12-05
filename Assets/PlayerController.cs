8607d004f70a70aa0f0a6b477a798c6395ebe6a7
OS-RAVEN <Chaushin59@gmail.com>
2023-12-05 14:54:04 +0900

    First Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Joystick joystick;
+    public float WorkSpeed = 0.1f;
+    public float SlideSpeed = 0.1f;
+    public float Countdown =10.0f;
+
+
+
+    void Update()
+    {
+        float x = joystick.Horizontal * SlideSpeed;
+        float y = joystick.Vertical * WorkSpeed;
+
+        Countdown -= Time.deltaTime;
+
+        /*お腹が空いたとき*/
+        if (Countdown <= 3f && Countdown >= 0f)
+        {
+            transform.position += new Vector3(x, 0, y + 0.01f);
+        }
+        /*空腹で動けない*/
+        else if (Countdown <= 0f)
+        {
+            transform.position += new Vector3(0, 0, 0);
+        }
+        /*通常時*/
+        else
+        {
+            transform.position += new Vector3(x, 0, y);
+        }
+
+    }
+}
